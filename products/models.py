fba5f965e09929b5039160e66fbe331a89c26008
Elaine Archbold <e.t.archbold@gmail.com>
2020-04-18 15:29:45 +0000

    started styling

--- products/models.py
+++ products/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+CATEGORY_CHOICES = (
+    ('Baby', 'BABY PRINTS'),
+    ('Christening Prints', 'CHRISTENING PRINTS'),
+    ('Engagement Prints', 'ENGAGEMENT PRINTS'),
+    ('Wedding Prints', 'WEDDING PRINTS'),
+    ('Family Prints', 'FAMILY PRINTS'),
+)
+
 
 class Product(models.Model):
+    category = models.CharField(max_length=30, choices=CATEGORY_CHOICES, default='baby prints')
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
