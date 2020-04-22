e6f1331c017a532b5cb11d8409981302651c823d
Elaine Archbold <e.t.archbold@gmail.com>
2020-04-22 20:05:16 +0000

    styled checkout and cart pages, made amendments to checkot form

--- checkout/views.py
+++ checkout/views.py
@@ -46,7 +46,7 @@ def checkout(request):
                 messages.error(request, "Your card was declined!")
 
             if customer.paid:
-                messages.error(request, "You have successfully paid")
+                messages.error(request, "You have successfully paid! You will receive a confirmation email shortly")
                 request.session['cart'] = {}
                 return redirect(reverse('products'))
             else:
--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from .models import Product
 
 
 def all_products(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('category')
     return render(request, "products.html", {"products": products})
 
 
