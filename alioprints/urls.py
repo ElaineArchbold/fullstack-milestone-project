13ec722f70ed90c0c4bd96c5cefbabe7e86f9423
Elaine Archbold <e.t.archbold@gmail.com>
2020-04-25 17:21:34 +0000

    added index

--- alioprints/urls.py
+++ alioprints/urls.py
@@ -28,7 +28,7 @@ from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home, name='home'),
+    url(r'^$', home, name='index'),
     url(r'^contact/', include(urls_home)),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
