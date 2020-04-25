7cd649f403b15e50ce198fafffd9049375fbbc5d
Elaine Archbold <e.t.archbold@gmail.com>
2020-04-25 16:09:20 +0000

    finished styling home page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import index, home
+from .views import index, home, contact
 
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^index', index, name='index'),
+    url(r'^contact', contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -12,3 +12,8 @@ def home(request):
 def index(request):
     """A view that displays the index page"""
     return render(request, "index.html")
+
+
+def contact(request):
+    """A view that displays the contact page"""
+    return render(request, "contact.html")
