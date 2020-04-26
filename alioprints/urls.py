582569cd61afd7eb259e076a284918da8103edac
Elaine Archbold <e.t.archbold@gmail.com>
2020-04-26 19:10:42 +0000

    changed galleryview

--- alioprints/urls.py
+++ alioprints/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from accounts import urls as urls_accounts
 from products import urls as urls_products
 from home import urls as urls_home
+from home.views import home
 from cart import urls as urls_cart
 from search import urls as urls_search
 from checkout import urls as urls_checkout
@@ -27,8 +28,7 @@ from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include(urls_home)),
-    url(r'^home/', include(urls_home)),
+    url(r'^$', home, name="home"),
     url(r'^contact/', include(urls_home)),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
